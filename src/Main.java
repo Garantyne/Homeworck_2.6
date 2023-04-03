bb5e5e41c44598b4b3e7f6940ec39c6ef89c91e3
Андрей <krel.andrej@yandex.ru>
2023-04-03 20:03:00 +0700

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
 
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) % 2 != 0) {
+                System.out.print(nums.get(i)+" ");
+            }
+        }
+        System.out.println();
+        Collections.sort(nums);
+        int prevNum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num % 2 == 0 && num != prevNum) {
+                System.out.print(num + " ");
+                prevNum = num;
+            }
+        }
+        System.out.println();
+        List<String> set1 = new ArrayList<>(List.of("qwer", "asdf", "qwer", "zxcv", "trt", "asdf"));
+        Set<String> set2 = new HashSet<>(set1);
+        System.out.println(set2);
+
+        System.out.println(set1.size() - set2.size());
     }
 }
